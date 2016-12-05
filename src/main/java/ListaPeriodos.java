ad2916f49df86bfc8361d605e19af7d646c11188
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-05 21:28:20 -0200

    Lista de Periodos, agora valida periodos intercalados e periodos que cobrem o proximo periodo

--- src/main/java/ListaPeriodos.java
+++ src/main/java/ListaPeriodos.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,9 +11,7 @@ public class ListaPeriodos {
     private List<Periodo> periodos;
 
     public ListaPeriodos(List<Periodo> lista){
-
-        this.setPeriodos(this.ordenarLista(this.validarLista(lista)));
-
+        setPeriodos(validarListaOrdenada(ordenarLista(filtrarPeriodosValidos(lista))));
     }
 
     private List<Periodo> ordenarLista(List<Periodo> lista){
@@ -22,10 +19,46 @@ public class ListaPeriodos {
         return lista;
     }
 
-    private List<Periodo> validarLista(List<Periodo> lista){
+    private List<Periodo> filtrarPeriodosValidos(List<Periodo> lista){
         return lista.stream()
                 .filter((p) -> p.valid())
                 .collect(Collectors.toList());
+    }
+
+    private Periodo intecalarPeriodos(Periodo p1, Periodo p2){
+        return new Periodo(p1.getDataInicial(), p2.getDataFinal());
+    }
+
+    private boolean validarPeriodoCobreOutro( Periodo p1, Periodo p2){
+        return p1.getDataFinal().isAfter(p2.getDataInicial())
+                && p1.getDataFinal().isAfter(p2.getDataFinal());
+    }
+
+    private boolean validarPeriodosIntercalados( Periodo p1, Periodo p2){
+        return p1.getDataFinal().isAfter(p2.getDataInicial()) && !validarPeriodoCobreOutro(p1, p2);
+    }
+
+    private void preencherListaPeriodosValidos(List<Periodo> lista, Periodo p1, Periodo p2){
+        if(validarPeriodoCobreOutro(p1,p2)){
+            if( lista.indexOf(p1) == -1 ) lista.add(p1);
+        }
+        else if(validarPeriodosIntercalados(p1, p2)){
+            if( lista.indexOf(p1) > -1) lista.remove(p1);
+            lista.add(intecalarPeriodos(p1, p2));
+        }else{
+            if( lista.indexOf(p1) == -1 )lista.add(p1);
+            lista.add(p2);
+        }
+    }
+
+    private List<Periodo> validarListaOrdenada( List<Periodo> listaOrdenada ){
+        List<Periodo> listaValida = new ArrayList<>();
+        for (int i = 0; i < listaOrdenada.size() - 1; i++) {
+            Periodo p1 = i == 0 ? listaOrdenada.get(i) : listaValida.get(listaValida.size() - 1);
+            Periodo p2 = listaOrdenada.get(i+1);
+            preencherListaPeriodosValidos(listaValida, p1, p2);
+        }
+        return listaValida;
     }
 
     public List<Periodo> getPeriodos() {
